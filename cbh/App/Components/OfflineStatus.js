0938bb77c8b6c96b7df9dfb6d07822a4d6ad6150
Gary Davies <garydavieshome@gmail.com>
2019-01-05 16:28:27 +0800

    OfflineStatus

--- cbh/App/Components/OfflineStatus.js
+++ cbh/App/Components/OfflineStatus.js
@@ -1,60 +1,75 @@
 import React, { Component } from 'react';
 import { View, Text, NetInfo, Dimensions, StyleSheet } from 'react-native';
-import {ScaleUsingViewWidth, ScaleFont} from '../Helper/DetermineFontScale';
+import {DetermineFontScale, ScaleUsingViewWidth, ScaleFont} from '../Helper/DetermineFontScale';
 import { Colors } from '../Themes/Colors';
 import { Fonts } from '../Themes/Fonts';
 
 const { width } = Dimensions.get('window');
 
 class OfflineStatus extends Component {
-  state = {
-    isConnected: true
-  };
+    state = {
+        isConnected: true
+    };
 
-  componentDidMount() {
-    NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectivityChange);
-  }
-
-  componentWillUnmount() {
-    NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
-  }
-
-  handleConnectivityChange = isConnected => {
-    if (isConnected) {
-      this.setState({ isConnected: true });
-    } else {
-      this.setState({ isConnected: false });
+    constructor() {
+        super();
     }
-  };
-  
-  render() {
-    if (!this.state.isConnected) {
-      let styles = StyleSheet.create({
-        offlineContainer: {
-          backgroundColor: Colors.red,
-          height: ScaleUsingViewWidth(0.08),
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'row',
-          width: ScaleUsingViewWidth(1.0),
-          position: 'absolute',
-          top: ScaleUsingViewWidth(this.props.top)
-        },
-        offlineText: { 
-          fontSize: ScaleFont(11.0),
-          fontFamily: Fonts.mediumFont,
-          color: '#fff'
+
+    componentDidMount() {
+        Dimensions.addEventListener( 'change', () => {
+            this.change = this.change + 1;
+            this.getOrientation();
+        });
+        NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectivityChange);
+  }
+
+    componentWillUnmount() {
+        NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
+    }
+
+
+    getOrientation = () => {
+        this.setState({
+            fontScale: DetermineFontScale()
+        });
+    }
+
+    handleConnectivityChange = isConnected => {
+        if (isConnected) {
+            this.setState({ isConnected: true });
+        } else {
+            this.setState({ isConnected: false });
         }
-      });
+    };
+  
+    render() {
+        if (!this.state.isConnected) {
+            let styles = StyleSheet.create({
+                offlineContainer: {
+                    backgroundColor: Colors.red,
+                    height: ScaleUsingViewWidth(0.08),
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'row',
+                    width: ScaleUsingViewWidth(1.0),
+                    position: 'absolute',
+                    top: ScaleUsingViewWidth(this.props.top)
+                },
+                offlineText: { 
+                    fontSize: ScaleFont(11.0),
+                    fontFamily: Fonts.mediumFont,
+                    color: '#fff'
+                }
+            });
 
-      return (
-        <View style={styles.offlineContainer}>
-          <Text style={styles.offlineText}>No Internet Connection</Text>
-        </View>  
-      );
+            return (
+                <View style={styles.offlineContainer}>
+                    <Text style={styles.offlineText}>No Internet Connection</Text>
+                </View>  
+            );
+        }
+        return null;
     }
-    return null;
-  }
 }
 
 export default OfflineStatus;
